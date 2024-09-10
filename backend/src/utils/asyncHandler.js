2bc98b110ddf070a128173c88e7fad390c567fc5
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-10 12:03:54 +0530

    fix a bug in asynchandler

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,3 +1,3 @@
-const asyncHandler = async (requestHandler) => async (req, res, next) => Promise.resolve(requestHandler(req, res)).catch(next());
+const asyncHandler = async (requestHandler) => async (req, res, next) => Promise.resolve(requestHandler(req, res)).catch(next);
 
 export {asyncHandler}
