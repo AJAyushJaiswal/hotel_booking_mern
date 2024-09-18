6666cc8a02c8174b49a2d6e6a03f698224442ba6
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-18 21:53:32 +0530

    fix bug to ensure cookies are actually stored on the client browser

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    // credentials: true
+    credentials: true
 }));
 console.log(process.env.CORS_ORIGIN);
 
--- frontend/src/apiService.js
+++ frontend/src/apiService.js
@@ -4,6 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const createUser = async (formData) => {
     const response = await fetch(`${API_BASE_URL}/api/v1/users/register`, {
         method: 'POST',
+        credentials: 'include',
         headers: {
             "Content-Type": "application/json"
         },
