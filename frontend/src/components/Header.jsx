4df125e9c3d4cdf6bc3d3112ff26d9f40932f06f
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-23 12:17:40 +0530

    update header component to change navbar links if the user is logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,9 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
+import {useAppContext} from '../contexts/AppContext';
 
 export default function Header(){
+    const {isLoggedIn} = useAppContext();
+
     return (
         <header className="shadow-md py-5">
             <div className="container mx-auto flex justify-between items-center">
@@ -9,15 +12,51 @@ export default function Header(){
                         HotelGod
                     </Link>
                 </div>
+
+                <div className="flex">
+                    { isLoggedIn ?
+                        <ul className="flex">
+                            <li className="mx-2">
+                                <NavLink to="/bookings" className={`text-lg font-semibold ${({isActive}) => isActive && 'text-violet-600'}`}>My Bookings</NavLink>
+                            </li>
+                            <li className="mx-2">
+                                <NavLink to="/hotels" className={`text-lg font-semibold ${({isActive}) => isActive && 'text-violet-600'}`}>My Hotels</NavLink>
+                            </li>
+                            <li className="mx-2">
+                                <NavLink to="/profile" className={`text-lg font-semibold ${({isActive}) => isActive && 'text-violet-600'}`}>Profile</NavLink>
+                            </li>
+                        </ul>
+                    :
+                        <ul className="flex">
+                            <li className="mx-2">
+                                <Link to="/#hotels" className="text-lg font-semibold">Hotels</Link>
+                            </li>
+                            <li className="mx-2">
+                                <Link to="/#offers" className="text-lg font-semibold">Offers</Link>
+                            </li>
+                            <li className="mx-2">
+                                <Link to="/#testimonials" className="text-lg font-semibold">Testimonials</Link>
+                            </li>
+                            <li className="mx-2">
+                                <Link to="/#contact_us" className="text-lg font-semibold">Contact Us</Link>
+                            </li>
+                        </ul>
+                    }
+                </div>
+        
+                {isLoggedIn ? 
+                    <button to="/logout" className="bg-violet-600 text-white px-5 py-1.5 text-lg font-semibold rounded-3xl hover:bg-violet-700 active:bg-violet-800 ">Logout</button>
+                : 
+                    <ul className="flex">
+                        <li className="mx-2">
+                            <Link to="/register" className="bg-violet-600 text-white px-5 py-1.5 text-lg font-semibold rounded-3xl hover:bg-violet-700 active:bg-violet-800 focus:ring-red-800">Register</Link>
+                        </li>
+                        <li className="mx-2">
+                            <Link to="/login" className="bg-violet-600 text-white px-5 py-1.5 text-lg font-semibold rounded-3xl hover:bg-violet-700 active:bg-violet-800 ">Login</Link>
+                        </li>
+                    </ul>
+                }
             
-                <ul className="flex">
-                    <li className="mx-2">
-                        <Link to="/register" className="bg-violet-600 text-white px-5 py-1.5 text-lg font-semibold rounded-3xl hover:bg-violet-700 active:bg-violet-800 focus:ring-red-800">Register</Link>
-                    </li>
-                    <li className="mx-2">
-                        <Link to="/login" className="bg-violet-600 text-white px-5 py-1.5 text-lg font-semibold rounded-3xl hover:bg-violet-700 active:bg-violet-800 ">Login</Link>
-                    </li>
-                </ul>
             </div>
         </header>
     );
