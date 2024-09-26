6fa3ddced0f9928a4cc4e116ef1ad5cd5e9022ab
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-26 13:32:51 +0530

    fix bugs in add hotel controller and route

--- backend/src/controllers/hotel.controller.js
+++ backend/src/controllers/hotel.controller.js
@@ -2,6 +2,8 @@ import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from '../utils/ApiError.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
 import {Hotel} from '../models/hotel.model.js';
+import {validationResult} from 'express-validator';
+import {uploadToCloudinary} from '../utils/cloudinary.js';
 
 
 const createHotel = asyncHandler(async (req, res) => {
@@ -26,7 +28,7 @@ const createHotel = asyncHandler(async (req, res) => {
         throw new ApiError(400, 'Failed to upload images!');
     }
 
-    const hotel = await Hotel.create({name, address, city, country, type, description, starRating, contactNo, email, owner: req.user._id, email, facilities});
+    const hotel = await Hotel.create({name, address, city, country, type, description, starRating, contactNo, email, owner: req.user._id, email, facilities, images: uploadUrls});
     
     if(!hotel){
         throw new ApiError(400, 'Failed to add hotel!');
--- backend/src/routes/hotel.routes.js
+++ backend/src/routes/hotel.routes.js
@@ -1,44 +1,49 @@
 import {Router} from 'express';
+import {verifyAccessToken} from '../middlewares/auth.middleware.js';
 import {createHotel} from '../controllers/hotel.controller.js';
 import {body} from 'express-validator';
+import {upload} from '../middlewares/multer.middleware.js';
 
 
 
 const router = Router();
 
 
-router.route('/add').post(verifyAccessToken, [
-    body('name')
-    .notEmpty().withMessage('Hotel name is required!'),
+router.route('/add').post(verifyAccessToken, 
+    upload.array('images', 6),
+    [
+        body('name')
+        .notEmpty().withMessage('Hotel name is required!'),
 
-    body('address')
-    .isLength({max: 50}).withMessage('Address can\'t be more than 50 characters!'),
+        body('address')
+        .isLength({max: 50}).withMessage('Address can\'t be more than 50 characters!'),
 
-    body('city')
-    .notEmpty().withMessage('City name is required!'),
+        body('city')
+        .notEmpty().withMessage('City name is required!'),
 
-    body('country')
-    .isIn(['India', 'Sri Lanka', 'US', 'UK', 'Japan', 'South Korea', 'UAE', 'Egypt', 'South Africa', 'Maldives', 'Bahamas']).withMessage('Invalid option! Select from the listed countries'),
+        body('country')
+        .isIn(['India', 'Sri Lanka', 'US', 'UK', 'Japan', 'South Korea', 'UAE', 'Egypt', 'South Africa', 'Maldives', 'Bahamas']).withMessage('Invalid option! Select from the listed countries'),
 
-    body('type')
-    .isIn(['budget', 'boutique', 'luxury', 'resort', 'apartment', 'spa', 'vacation', 'business', 'hostel']).withMessage('Invalid option! Select from the listed hotel types'),
+        body('type')
+        .isIn(['budget', 'boutique', 'luxury', 'resort', 'apartment', 'spa', 'vacation', 'business', 'hostel']).withMessage('Invalid option! Select from the listed hotel types'),
 
-    body('description')
-    .isLength({max: 200}).withMessage('Description can\'t be more than 200 characters!'),
+        body('description')
+        .isLength({max: 200}).withMessage('Description can\'t be more than 200 characters!'),
 
-    body('starRating')
-    .isIn([1, 2, 3, 4, 5]).withMessage('Star rating can only be an integer in range of 1-5!')
-    .optional(),
+        body('starRating')
+        .isIn([1, 2, 3, 4, 5]).withMessage('Star rating can only be an integer in range of 1-5!')
+        .optional(),
 
-    body('contactNo').notEmpty(),
+        body('contactNo').notEmpty(),
 
-    body('email')
-    .isEmail().withMessage('Invalid email format!')
-    .optional(),
+        body('email')
+        .isEmail().withMessage('Invalid email format!')
+        .optional(),
 
-    body('facilities').notEmpty()
-], createHotel);
-
+        body('facilities').notEmpty()
+    ], 
+    createHotel
+);
 
 
 export default router;
