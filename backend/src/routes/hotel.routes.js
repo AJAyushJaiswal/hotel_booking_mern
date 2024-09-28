c0e2a94ed1e70ebac8be779589eb8a83d64a74fb
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-09-28 16:42:00 +0530

    fix some bugs in input validation of create hotel controller

--- backend/src/routes/hotel.routes.js
+++ backend/src/routes/hotel.routes.js
@@ -16,31 +16,34 @@ router.route('/add').post(verifyAccessToken,
         .notEmpty().withMessage('Hotel name is required!'),
 
         body('address')
+        .notEmpty().withMessage('Address is required!')
         .isLength({max: 50}).withMessage('Address can\'t be more than 50 characters!'),
 
         body('city')
-        .notEmpty().withMessage('City name is required!'),
+        .notEmpty().withMessage('City is required!'),
 
         body('country')
-        .isIn(['India', 'Sri Lanka', 'US', 'UK', 'Japan', 'South Korea', 'UAE', 'Egypt', 'South Africa', 'Maldives', 'Bahamas']).withMessage('Invalid option! Select from the listed countries'),
+        .isIn(['India', 'Sri Lanka', 'US', 'UK', 'Japan', 'South Korea', 'UAE', 'Egypt', 'South Africa', 'Maldives', 'Bahamas']).withMessage('Invalid country option! Select from the listed countries'),
 
         body('type')
-        .isIn(['budget', 'boutique', 'luxury', 'resort', 'apartment', 'spa', 'vacation', 'business', 'hostel']).withMessage('Invalid option! Select from the listed hotel types'),
+        .isIn(['budget', 'boutique', 'luxury', 'resort', 'apartment', 'spa', 'vacation', 'business', 'hostel']).withMessage('Invalid hotel type! Select from the listed hotel types'),
 
         body('description')
+        .notEmpty().withMessage('Description is required!')
         .isLength({max: 200}).withMessage('Description can\'t be more than 200 characters!'),
 
         body('starRating')
-        .isIn([1, 2, 3, 4, 5]).withMessage('Star rating can only be an integer in range of 1-5!')
+        .trim()
+        .isIn([1, 2, 3, 4, 5, '']).withMessage('Star rating can only be an integer in range of 1-5!')
         .optional(),
 
-        body('contactNo').notEmpty(),
+        body('contactNo').notEmpty().withMessage('Contact no. is required!'),
 
         body('email')
         .isEmail().withMessage('Invalid email format!')
         .optional(),
 
-        body('facilities').notEmpty()
+        body('facilities').optional()
     ], 
     createHotel
 );
