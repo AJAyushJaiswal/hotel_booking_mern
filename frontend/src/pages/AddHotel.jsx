8805edcae03e43eb149da1a42faf43b27aa5ff76
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-07 00:56:14 +0530

    update add hotel component to redirect to my hotels page if hotel is added successfully

--- frontend/src/pages/AddHotel.jsx
+++ frontend/src/pages/AddHotel.jsx
@@ -2,13 +2,17 @@ import ManageHotelForm from "../forms/HotelForm/ManageHotelForm.jsx";
 import {useMutation} from 'react-query';
 import {addHotel} from '../api-services/hotel.api-services.js';
 import {useAppContext} from '../contexts/AppContext.jsx';
+import {useNavigate} from "react-router-dom";
 
 export default function AddHotel(){
     const {showToast} = useAppContext();
+    
+    const navigate = useNavigate();
 
     const {mutate, isLoading} = useMutation(addHotel, {
         onSuccess: (result) => {
             showToast({message: result.message, success:result.success});
+            navigate('/hotels');
         },
         onError: (error) => {
             showToast({message: error.message, success: false});
