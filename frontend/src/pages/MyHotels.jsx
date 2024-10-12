56ea9b3679be95e9de098107c9a6040b7d26b320
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-12 23:05:47 +0530

    add deletion functionality to buttons to delete hotels

--- frontend/src/pages/MyHotels.jsx
+++ frontend/src/pages/MyHotels.jsx
@@ -1,6 +1,7 @@
-import {useQuery} from 'react-query';
+import {useQuery, useMutation} from 'react-query';
 import {Link} from 'react-router-dom';
-import {getMyHotels} from '../api-services/hotel.api-services.js';
+import {getMyHotels, deleteHotel} from '../api-services/hotel.api-services.js';
+import {useAppContext} from '../contexts/AppContext.jsx';
 
 
 export default function MyHotels(){
@@ -10,8 +11,23 @@ export default function MyHotels(){
         }
     });
     
+    const {showToast} = useAppContext();
+    
+    const {mutate} = useMutation(deleteHotel, {
+        onSuccess: async (result) => {
+            showToast({message: result.message, success: result.success});
+        },
+        onError: async (error) => {
+            showToast({message: error.message, success: false});
+        }
+    });
+    
+    const deleteHotelClick = (hotelId) => {
+        mutate(hotelId);
+    }
+    
     return (
-        <div className="my-16 w-4/5 m-auto">
+        <div className="my-16">
             <div className="flex justify-between">
                 <h1 className="text-2xl font-bold text-center">My Hotels</h1>
                 <div className="sticky top-20 right-10">
@@ -28,7 +44,7 @@ export default function MyHotels(){
                             <div className="flex justify-between">
                                 <div className="mb-4">
                                     <h3 className="text-lg font-semibold leading-3">{hotel.name}</h3> 
-                                    <span className="text-sm">{hotel.address}, {hotel.city},{hotel.country}</span> 
+                                    <span className="text-sm">{hotel.address}, {hotel.city}, {hotel.country}</span> 
                                 </div>
                                 <div className="flex">
                                     {/* View Full Details Button */}
@@ -51,7 +67,7 @@ export default function MyHotels(){
                                         </svg>
                                     </Link>
                                     {/* Delete Hotel Button */}
-                                    <button className='self-start'>
+                                    <button className='self-start' onClick={() => deleteHotelClick(hotelData.data._id)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={18} fill="rgb(134, 58, 238)"><path d="M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
                                     </button>
                                 </div>
