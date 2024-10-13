5d654d318ae653625eda2e435bc1e49ab48aae66
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-13 21:51:07 +0530

    rename fn for removing image url in edit hotel form to avoid confusion

--- frontend/src/forms/HotelForm/ImagesSection.jsx
+++ frontend/src/forms/HotelForm/ImagesSection.jsx
@@ -6,7 +6,7 @@ export default function ImagesSection(){
     
     const imageUrls = watch('images');
     
-    const deleteImage = (imageUrl) => {
+    const removeImageUrl = (imageUrl) => {
         setValue('images', imageUrls.filter(image => imageUrl !== image));
     }
 
@@ -18,7 +18,7 @@ export default function ImagesSection(){
                     {imageUrls?.map((url, index) => (
                         <div className="relative" key={index}>
                             <img src={url} alt={`Hotel Image ${index}`} className="min-w-full min-h-full object-cover border border-gray-100 shadow"/>
-                            <button className="absolute -top-1.5 -right-1.5 border border-gray-400 rounded-full p-0.5" onClick={() => deleteImage(url)}>
+                            <button className="absolute -top-1.5 -right-1.5 border border-gray-400 rounded-full p-0.5" onClick={() => removeImageUrl(url)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width={10} height={10} fill="gray"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
                             </button>
                         </div>
