c4f90f0ef1167916a352bf3c82bf816e960bfe93
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-10-28 12:12:18 +0530

    remove try catch from update hotel controller as its unnecessary

--- backend/src/controllers/hotel.controller.js
+++ backend/src/controllers/hotel.controller.js
@@ -99,38 +99,28 @@ const updateHotel = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Images must be in the range of 3-6!");
     }
     
-    try{
-        const imageUploadPromises = imageFiles.map(image => uploadToCloudinary(image));
-        const newImageUrls = await Promise.all(imageUploadPromises);
-        
-        const hotelUpdateResult = await Hotel.updateOne({_id: hotelId, owner: req.user._id}, {
-            name, address, city, country, description, type, 
-            starRating: starRating || null, 
-            contactNo, 
-            email: email || null, 
-            facilities: facilities || [], 
-            images: [...(imageUrls || []), ...newImageUrls]
-        }).lean();
-
-        if(hotelUpdateResult.modifiedCount === 0){
-            throw new ApiError(400, "Error updating hotel!");
-        }
-
-        const imagesToDelete = currentHotel.images?.filter((url) => !imageUrls?.includes(url)) || [];
-        
-        const imageDeletePromises = imagesToDelete.map((url) => deleteFromCloudinary(url));
-        await Promise.all(imageDeletePromises);
+    const imageUploadPromises = imageFiles.map(image => uploadToCloudinary(image));
+    const newImageUrls = await Promise.all(imageUploadPromises);
     
-        res.status(200).json(new ApiResponse(200, null, "Hotel updated successfully!"));
+    const hotelUpdateResult = await Hotel.updateOne({_id: hotelId, owner: req.user._id}, {
+        name, address, city, country, description, type, 
+        starRating: starRating || null, 
+        contactNo, 
+        email: email || null, 
+        facilities: facilities || [], 
+        images: [...(imageUrls || []), ...newImageUrls]
+    }).lean();
+
+    if(hotelUpdateResult.modifiedCount === 0){
+        throw new ApiError(400, "Error updating hotel!");
     }
-    catch(error){
-        console.log(error);
-        if(error instanceof ApiError){
-            throw new ApiError(error.statusCode, error.message);
-        }    
-        
-        throw new ApiError(500, "Internal Server Error!");
-    }
+
+    const imagesToDelete = currentHotel.images?.filter((url) => !imageUrls?.includes(url)) || [];
+    
+    const imageDeletePromises = imagesToDelete.map((url) => deleteFromCloudinary(url));
+    await Promise.all(imageDeletePromises);
+
+    res.status(200).json(new ApiResponse(200, null, "Hotel updated successfully!"));
 });
 
 
