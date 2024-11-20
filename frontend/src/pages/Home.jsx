6f0309a1665bf5ab4e928de92c5ff3870666f571
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-20 23:48:56 +0530

    add search form to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Section from '../components/Section.jsx';
+import SearchForm from '../forms/SearchForm/SearchForm.jsx';
 
 export default function Home(){
     return (
         <div className="my-20 w-full">
-            <div className="">Search Section</div>
+            <div className="mb-24 flex flex-col items-center">
+                <p className="text-2xl font-semibold mb-14">Book your favorite hotel</p> 
+                <div>
+                    <SearchForm/>
+                </div>
+            </div>
             <Section title="Recommended For You"/>
             <Section title="Top rated hotels"/>
             <Section title="Popular Destinations"/>
