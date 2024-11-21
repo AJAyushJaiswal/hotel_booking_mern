82558b117c12db5d6e7e26f594ba9ed5723599fd
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-21 22:42:36 +0530

    create api service to search hotels to book

---
+++ frontend/src/api-services/search.api-services.js
@@ -0,0 +1,30 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+
+const searchHotels = async (location, adultCount, childCount, checkInDate, checkOutDate, roomCount, pageNumber=1) => {
+    const query = new URLSearchParams({
+        location,
+        adultCount,
+        childCount,
+        checkInDate,
+        checkOutDate,
+        roomCount,
+        pageNumber
+    }).toString();
+
+    const response = await fetch(`${API_BASE_URL}/api/v1/search?${query}`, {
+        method: 'GET',
+        credentials: 'include'
+    });
+    
+    const result = await response.json();
+    
+    if(!result.status){
+        throw new Error(result.message);
+    }
+    
+    return result;
+}
+
+export {
+    searchHotels
+};
