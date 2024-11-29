3d2ae841000c13581cf8f1411ebfdf2a6df83094
Ayush Jaiswal <ayushjaiswal3108@gmail.com>
2024-11-29 11:39:23 +0530

    add room facilities and views lists

---
+++ backend/src/constants/room.constants.js
@@ -0,0 +1,32 @@
+const roomFacilitiesList = [
+    'Bathroom',
+    'A/C',
+    'Closet',
+    'Balcony',
+    'Work Area',
+    'Television',
+    'Minibar',
+    'Phone',
+    'Room Service'
+];
+
+
+const roomViewsList = [
+    'No View',
+    'Lake View',
+    'Pool View',
+    'City View',
+    'Ocean View',
+    'River View',
+    'Forest View',
+    'Garden View',
+    'Skyline View',
+    'Mountain View',
+    'Historic View',
+];
+
+
+export {
+    roomFacilitiesList,
+    roomViewsList
+}
